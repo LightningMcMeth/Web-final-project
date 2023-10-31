a21fcba515215f89a458bfa00743a3f88c8502a0
LightningMcMeth <mhlibovytskyy@kse.org.ua>
2023-10-31 21:31:27 +0200

    Finished the main page. From now on pages will be easier to make, as some of the elements will be reused from here.

--- scripts/script.js
+++ scripts/script.js
@@ -35,3 +35,26 @@ window.onload = () => {
         imageIndex = (imageIndex + 1) % images.length;
     }, 10000);
 };
+
+
+document.addEventListener('submit', (event) => {
+    if (event.target.matches('#email-form')) {
+        event.preventDefault();
+
+        let emailInputElement = document.getElementById('email-input');
+        let emailInput = emailInputElement.value;
+        let gmailRegex = /^[a-zA-Z0-9._-]+@gmail\.com$/;
+
+        if (gmailRegex.test(emailInput)) {
+
+            console.log("Valid Gmail address");
+
+            emailInputElement.value = "";
+            emailInputElement.placeholder = "Email submitted!";
+        } else {
+
+            console.log("Invalid Gmail address");
+            alert("Please enter a valid Gmail address.");
+        }
+    }
+});
